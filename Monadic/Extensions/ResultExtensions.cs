3a4889fad92e53ac7f3fcd621093d16e0dda1269
Gabriel Ekblad <gabriel@ekblad.cc>
2018-12-10 20:41:28 +0100

    Added an extension to Result that can throw a produced exception iff the result represents an unsuccessful result.

---
+++ Monadic/Extensions/ResultExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Monadic.Extensions
+{
+    public static class ResultExtensions
+    {
+        public static void ThrowIfUnsuccessful(this Result result, Func<IEnumerable<Error>, Exception> ex)
+        {
+            if (!result.Succeeded)
+            {
+                throw ex(result.Errors);
+            }
+        }
+    }
+}
